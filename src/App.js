768d8263d0dbd749ad8259b4c9424c499af9b7e4
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-09-20 00:34:56 -0400

    accessing contract data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import React from "react";
 
-import web3 from './web3.js';
+import web3 from './web3';
+import contract from './lottery';
 
 class App extends React.Component {
   render() {
@@ -9,10 +10,13 @@ class App extends React.Component {
     web3.eth.getAccounts()
       .then(console.log(`Web3 version: ${web3.version}`));
 
+      // Test console logs
+      console.log(contract.options.address);
+
     return (
       <div className="App">
         <h1>Eth-Lottery</h1>
-
+        <p>Lottery deployed at: <strong>{contract.options.address}</strong></p>
       </div>
     );
   }
