b673c7201c92da40026b767c35a5ccd7724065bd
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-09-20 00:51:10 -0400

    es6 update for state initialization

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import "./App.css";
 import React from "react";
 
 import web3 from './web3';
-import contract from './lottery';
+import lotteryContract from './lottery';
 
 class App extends React.Component {
+
+  state = { // Don't forget to initialize state
+    manager: '' 
+  };
+
+  async componentDidMount() {
+    const contractManager = await lotteryContract.methods.manager().call(); // Get current address of manager then add to state
+
+    this.setState({ manager: contractManager });
+  };
+
   render() {
 
     web3.eth.getAccounts()
       .then(console.log(`Web3 version: ${web3.version}`));
 
       // Test console logs
-      console.log(contract.options.address);
+      console.log(this.state.manager);
 
     return (
-      <div className="App">
-        <h1>Eth-Lottery</h1>
-        <p>Lottery deployed at: <strong>{contract.options.address}</strong></p>
+      <div>
+        <h2>Eth-Lottery</h2>
+        <p>Lottery manager: <strong>{this.state.manager}</strong></p>
       </div>
     );
   }
