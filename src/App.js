e377e77593f2318058ea1d3deb33752c8b5ae26b
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-09-20 01:04:21 -0400

    Reading data from contract instance and updating state/UI

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import lotteryContract from './lottery';
 class App extends React.Component {
 
   state = { // Don't forget to initialize state
-    manager: '' 
+    manager: '',
+    participantCount: 0,
+    prize: ''
   };
 
   async componentDidMount() {
     const contractManager = await lotteryContract.methods.manager().call(); // Get current address of manager then add to state
+    const players = await lotteryContract.methods.getParticipants().call();
+    const prize = await web3.eth.getBalance(lotteryContract.options.address);
 
-    this.setState({ manager: contractManager });
+    this.setState({ manager: contractManager, participantCount: players.length, prize: prize });
   };
 
   render() {
@@ -28,6 +32,10 @@ class App extends React.Component {
       <div>
         <h2>Eth-Lottery</h2>
         <p>Lottery manager: <strong>{this.state.manager}</strong></p>
+        <p>
+          There are currently: <strong>{this.state.participantCount}</strong> 
+          participant(s) entered to win <strong>{web3.utils.fromWei(this.state.prize, 'ether')}</strong> ether.
+        </p>
       </div>
     );
   }
