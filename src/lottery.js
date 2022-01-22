7fa35cd3d0ac8cef29c890e77e212c5f854289ab
pdeerat <40063808+pdeerat@users.noreply.github.com>
2022-01-22 22:14:29 +0700

    Update lottery.js

--- src/lottery.js
+++ src/lottery.js
@@ -1,7 +1,8 @@
 // Contract Address and ABI
 import web3 from './web3.js';
 
-const address = "0x62ab4EFe2AD745AA4AA31bCBf11CeC8Eb203DC04";
+//const address = "0x62ab4EFe2AD745AA4AA31bCBf11CeC8Eb203DC04";
+const address = "0xfa3346d959D89d774c82E71A65b7cfdfDba11B64";
 const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getParticipants","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"getWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enterLottery","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lastWinner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(abi, address); // Exports complete copy of our lottery contract on blockchain
